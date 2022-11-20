339bed0ae8e783f09c2b99b97bc7c452cca14c89
vibesx <lucian.paslariu@gmail.com>
2022-11-20 09:55:57 +0200

    intermediary commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-function App() {
-  return (
-    <div>
+import { Route, Switch, Redirect } from "react-router-dom";
 
-    </div>
-  );
+import AllQuotes from "./pages/AllQuotes";
+import NewQuotes from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+import Layout from "./components/layout/Layout";
+
+function App() {
+	return (
+		<Layout>
+			<div>
+				<Switch>
+					<Route path="/" exact>
+						<Redirect to="/quotes"></Redirect>
+					</Route>
+					<Route path="/quotes" exact>
+						<AllQuotes></AllQuotes>
+					</Route>
+					<Route path="/quotes/:quoteId">
+						<QuoteDetail></QuoteDetail>
+					</Route>
+					<Route path="/new-quote">
+						<NewQuotes></NewQuotes>
+					</Route>
+				</Switch>
+			</div>
+		</Layout>
+	);
 }
 
 export default App;
